c018ac7a26e7257c46e124cf6b65520cf453db7c
Fill13244556 <114169957+Fill13244556@users.noreply.github.com>
2022-12-08 18:40:24 +0300

    Add files via upload

---
+++ Settings.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+#include <SFML/Graphics.hpp>
+
+const float WINDOW_WIDTH = 800.f;
+const float WINDOW_HEIGTH = 600.f;
+const std::string WINDOW_TITLE = "SFML Lesson3";
+
+const float BAT_WIDTH = 20.f;
+const float BAT_HEIGTH = 80.f;
+const float BAT_OFFSET = 50.f;
+const sf::Color LEFT_BAT_COLOR{ sf::Color::Magenta };
+const sf::Color RIGHT_BAT_COLOR{ sf::Color::Red };
+const float FPS = 60.f; 
+const float BALL_RADIUS = 50.f;
+const sf::Color BALL_COLOR{ sf::Color::Yellow };
+const float  batDy = 5.f;
+const float batDx = 5.f; 
